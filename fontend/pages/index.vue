fe866963ca914d159e72bcbda54e68b56b712843
Quaomiuzzaman Kabbya <kabbya44@gmail.com>
2025-01-21 23:54:15 +0600

    desing day like facebook

--- fontend/pages/index.vue
+++ fontend/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto">
+    <div class="container mx-auto flex">
         <div class="w-2/3 xl:max-w-[800px] xl:mx-auto">
             <Day />
 
@@ -40,7 +40,22 @@
 
             <div v-if="postsStore.loading" class="loading">Loading...</div>
         </div>
-        <div class="w-1/3"> </div> 
+        <div class="w-1/3">
+            <div class="border border-2 border-gray-200 p-4">
+                <h2 class="text-gray-500 font-semibold text-xl"> Contact </h2>
+
+                <div class="mt-6">
+                    <div class="flex items-center">
+                        <div class="relative w-10 h-10 rounded-full overflow-hidden cursor-ponter">
+                            <img class="object-cover overflow-hidden"
+                                src="https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-1/460822526_2018752565210990_2603428692174454636_n.jpg?stp=c0.0.765.765a_cp0_dst-jpg_s40x40_tt6&_nc_cat=103&ccb=1-7&_nc_sid=1d2534&_nc_ohc=QG5eKjBoH8IQ7kNvgGPp8yI&_nc_zt=24&_nc_ht=scontent.fdac5-2.fna&_nc_gid=A8PoQZOpk9xXCmYKGTPR1eX&oh=00_AYC9lXQYGQBV6uIiE-a0tjMjjQIcK75bYe5tJHdofwEg6w&oe=6795C516" alt="">
+                        </div>
+
+                        <p class="text-gray-500 pl-3"> Name </p>
+                    </div>
+                </div>
+            </div>
+        </div> 
     </div>
 
     <div>
@@ -64,9 +79,9 @@
 import { usePostsStore } from '@/stores/posts';
 import { useNotificationStore } from '@/stores/notifications';
 import Day from '~/components/posts/Day.vue';
-// definePageMeta({
-//     middleware: 'auth',
-// });
+definePageMeta({
+    middleware: 'auth',
+});
 
 useHead({
     title:'Chating App',
